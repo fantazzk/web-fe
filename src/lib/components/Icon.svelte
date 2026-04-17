b9e2fbca8462cbb27f667312ce1029dcb745b1ab
TransparentDeveloper <2younsin@gmail.com>
2026-04-17 10:25:19 +0900

    기능(icon): #35 file-check 아이콘 추가

--- src/lib/components/Icon.svelte
+++ src/lib/components/Icon.svelte
@@ -12,7 +12,8 @@
 		| 'link'
 		| 'refresh-cw'
 		| 'play'
-		| 'info';
+		| 'info'
+		| 'file-check';
 
 	interface Props {
 		name: IconName;
@@ -97,5 +98,9 @@
 		<circle cx="12" cy="12" r="10" />
 		<line x1="12" y1="16" x2="12" y2="12" />
 		<line x1="12" y1="8" x2="12.01" y2="8" />
+	{:else if name === 'file-check'}
+		<path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" />
+		<path d="M14 2v4a2 2 0 0 0 2 2h4" />
+		<path d="m9 15 2 2 4-4" />
 	{/if}
 </svg>
